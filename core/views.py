c21f97469f2b1e6275723adecd1b43a6e58fc4d4
Dinara <d.shafigullina@itcodegroup.ru>
2023-07-25 22:37:38 +0500

    #2

--- core/views.py
+++ core/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from core.models import Phone
 
 
-def phones(request):
+def index(request):
+    return HttpResponse("<html><body>Начальная страница</body></html>")
+
+
+def phones_list(request):
     phones = [phone['name'] + '\n' for phone in Phone.objects.all().values()]
     if phones:
-        return HttpResponse(phones)
+        return render(request, 'core/templates/phones_list.html', context={'phones': phones})
     else:
-        return Http404('В базе пока нет моделей телефонов')
+        return redirect('')
 
+
+def phone(self, pk):
+    return HttpResponse(get_object_or_404(Phone, id=pk))
