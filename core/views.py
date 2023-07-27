a753227739cdabd691eb1a249326a1e1f1dee15b
dinara <d.shafigullina@itcodegroup.ru>
2023-07-27 11:31:14 +0500

    #2 Сделать задание по миграции и вьюхам

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_http_methods
 
 from core.models import Phone
 
@@ -8,6 +9,7 @@ def index(request):
     return HttpResponse("<html><body>Начальная страница</body></html>")
 
 
+@require_http_methods(['GET'])
 def phones_list(request):
     phones = [phone['name'] + '\n' for phone in Phone.objects.all().values()]
     if phones:
